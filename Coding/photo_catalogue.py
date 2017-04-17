fdda3c821b50692e875c86b28a802bf2d5c578c5
Maria Luiza Linhares Dantas <mdastro@users.noreply.github.com>
2017-04-17 19:09:21 -0300

    Add files via upload

--- Coding/photo_catalogue.py
+++ Coding/photo_catalogue.py
@@ -4,13 +4,26 @@
 """
     This program reads .csv files with the simulated galaxies and saves it in the correct format for BEAGLE to load and
     run,
+    # This version contains updated features. The main change is the transformation of flux units from
+    # "erg s^-1 cm^-2 Anstrom^-1" to "Jy"
     @author:  Maria Luiza L. Dantas
-    @date:    2017.03.08
-    @version: 0.0.1
+    @date:    2017.03.27
+    @version: 0.0.2
 """
 
 import numpy as np
 import pandas as pd
+
+
+# # My functions ---------------------------------------------------------------------------------------------------------
+# def flux_lambda_into_jy (flux_lambda, wavelength):
+#     """
+#     :param flux_lambda: this is the original flux in lambda, hence units in erg s^-1 cm^-2 Anstrom^-1
+#     :param wavelength: in our case, the effective wavelength (or pivot wavelength) of each J-PAS/J-PLUS photometric band
+#     :return flux_jy: the value returned is the original flux (now flux in nu), but converted to the unit jansky.
+#     """
+#     flux_jy = 3.34 * (10**4) * (wavelength**2) * flux_lambda
+#     return flux_jy
 
 # ======================================================================================================================
 # Main thread
@@ -18,17 +31,20 @@ import pandas as pd
 if __name__ == '__main__':
 
     # Loading the datasets ---------------------------------------------------------------------------------------------
-    my_data_z021        = np.loadtxt('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/total_file_z021.csv', delimiter=',',
-                                     dtype=str)[1:,:]
-    my_data_z050        = np.loadtxt('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/total_file_z050.csv', delimiter=',',
-                                     dtype=str)[1:,:]
-    my_data_z090        = np.loadtxt('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/total_file_z090.csv', delimiter=',',
-                                     dtype=str)
+    my_data_z021        = np.loadtxt('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/total_file_z021_fnu.csv',
+                                     delimiter=',', dtype=str)
+    my_data_z050        = np.loadtxt('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/total_file_z050_fnu.csv',
+                                     delimiter=',', dtype=str)
+    my_data_z090        = np.loadtxt('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/total_file_z090_fnu.csv',
+                                     delimiter=',', dtype=str)
     jpas_filters_header = np.loadtxt('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/header.txt', dtype=str)
-    jpas_filters_header = jpas_filters_header[:,1].T
-    my_data_sdss        = np.loadtxt('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/table_sdss_galaxies.txt', dtype=str)
-    sdss_redshifts      = np.loadtxt('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/redshifts.txt', dtype=str)[:,1]
-    sdss_filters_header = np.loadtxt('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/jpas_filters_header.txt', dtype=str)
+    jpas_filters_header = jpas_filters_header[:, 1].T
+    # my_data_sdss        = np.loadtxt('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/table_sdss_galaxies.txt', dtype=str)
+    # sdss_redshifts      = np.loadtxt('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/redshifts.txt', dtype=str)[:,1]
+    # sdss_filters_header = np.loadtxt('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/jpas_filters_header.txt', dtype=str)
+
+    # j_pas_effective_wavelength  =
+    # j_plus_effective_wavelength =
 
     # dictionary_z021 = {}
     # for i in range(len(my_data_z021[0, :])):                                   # Converting numpy array into dictionary
@@ -45,8 +61,8 @@ if __name__ == '__main__':
     filter_info_z090  = my_data_z090[0::2]
     filter_error_z090 = my_data_z090[1::2]
 
-    filter_info_sdss  = my_data_sdss[0::2]
-    filter_error_sdss = my_data_sdss[1::2]
+    # filter_info_sdss  = my_data_sdss[0::2]
+    # filter_error_sdss = my_data_sdss[1::2]
 
     # Creating a new header --------------------------------------------------------------------------------------------
     new_header = ['galaxy_id']
@@ -56,12 +72,12 @@ if __name__ == '__main__':
     new_header.append('redshift')                                                  # Appending objects' redshift.
     new_header = np.array(new_header)
 
-    new_sdss_header = ['galaxy_id']
-    for header in range(sdss_filters_header.size):
-        new_sdss_header.append(sdss_filters_header[header])
-        new_sdss_header.append('error_'+sdss_filters_header[header])
-    new_sdss_header.append('redshift')                                            # Appending objects' redshift.
-    new_sdss_header = np.array(new_sdss_header)
+    # new_sdss_header = ['galaxy_id']
+    # for header in range(sdss_filters_header.size):
+    #     new_sdss_header.append(sdss_filters_header[header])
+    #     new_sdss_header.append('error_'+sdss_filters_header[header])
+    # new_sdss_header.append('redshift')                                            # Appending objects' redshift.
+    # new_sdss_header = np.array(new_sdss_header)
 
     # Creating a new array with the info in the following order: "galaxy_id, photometry1, photo1_error, ..., redshift" -
     galaxies_z021 = []
@@ -94,36 +110,36 @@ if __name__ == '__main__':
     galaxies_z090 = np.array(galaxies_z090)
     galaxies_z090 = np.reshape(galaxies_z090, (filter_info_z090[:, 0].size, filter_info_z090[0, :].size * 2 + 2))
 
-    galaxies_sdss = []
-    for i in range(filter_info_sdss[:, 0].size):
-        galaxies_sdss.append(i+1)                   # appending the galaxy identifier (number)
-        for j in range(filter_info_sdss[0, :].size):
-            galaxies_sdss.append(filter_info_sdss[i][j])
-            galaxies_sdss.append(filter_error_sdss[i][j])
-        galaxies_sdss.append(sdss_redshifts[i])
-    galaxies_sdss = np.array(galaxies_sdss)
-    galaxies_sdss = np.reshape(galaxies_sdss, (filter_info_sdss[:, 0].size, filter_info_sdss[0, :].size * 2 + 2))
+    # galaxies_sdss = []
+    # for i in range(filter_info_sdss[:, 0].size):
+    #     galaxies_sdss.append(i+1)                   # appending the galaxy identifier (number)
+    #     for j in range(filter_info_sdss[0, :].size):
+    #         galaxies_sdss.append(filter_info_sdss[i][j])
+    #         galaxies_sdss.append(filter_error_sdss[i][j])
+    #     galaxies_sdss.append(sdss_redshifts[i])
+    # galaxies_sdss = np.array(galaxies_sdss)
+    # galaxies_sdss = np.reshape(galaxies_sdss, (filter_info_sdss[:, 0].size, filter_info_sdss[0, :].size * 2 + 2))
 
 
     # Stacking the new header on the top of the dataset ----------------------------------------------------------------
     new_galaxies_z021 = np.vstack((new_header, galaxies_z021))
     new_galaxies_z050 = np.vstack((new_header, galaxies_z050))
     new_galaxies_z090 = np.vstack((new_header, galaxies_z090))
-    new_galaxies_sdss = np.vstack((new_sdss_header, galaxies_sdss))
+    # new_galaxies_sdss = np.vstack((new_sdss_header, galaxies_sdss))
 
     # Saving the results using pandas ----------------------------------------------------------------------------------
     new_galaxies_z021 = pd.DataFrame(new_galaxies_z021)
-    new_galaxies_z021.to_csv('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/photocat_challenge_z021.csv', sep=',',
+    new_galaxies_z021.to_csv('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/photocat_challenge_z021_fnu.csv', sep=',',
                              header=None, index=False)
 
     new_galaxies_z050 = pd.DataFrame(new_galaxies_z050)
-    new_galaxies_z050.to_csv('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/photocat_challenge_z050.csv', sep=',',
+    new_galaxies_z050.to_csv('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/photocat_challenge_z050_fnu.csv', sep=',',
                              header=None, index=False)
 
     new_galaxies_z090 = pd.DataFrame(new_galaxies_z090)
-    new_galaxies_z090.to_csv('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/photocat_challenge_z090.csv', sep=',',
+    new_galaxies_z090.to_csv('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/photocat_challenge_z090_fnu.csv', sep=',',
                              header=None, index=False)
 
-    new_galaxies_sdss = pd.DataFrame(new_galaxies_sdss)
-    new_galaxies_sdss.to_csv('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/photocat_challenge_sdss.csv', sep=',',
-                             header=None, index=False)
+    # new_galaxies_sdss = pd.DataFrame(new_galaxies_sdss)
+    # new_galaxies_sdss.to_csv('/home/mldantas/Dropbox/DoutoradoIAG/Challenge/photocat_challenge_sdss.csv', sep=',',
+    #                          header=None, index=False)
