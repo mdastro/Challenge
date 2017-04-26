b587d71d71af8dc08dd195b41e98804ac177724a
Maria Luiza Linhares Dantas <mdastro@users.noreply.github.com>
2017-04-26 18:34:47 -0300

    Update beagle_fit_photometry.sh

--- CommandLineStuff/beagle_fit_photometry.sh
+++ CommandLineStuff/beagle_fit_photometry.sh
@@ -2,4 +2,5 @@ rm -rf $BEAGLE_RESULTS/fitting_example/BEAGLE-input-files
 BEAGLE --fit --parameter-file /home/beagle/Coding/BEAGLE-general/params/fit_photometry_example.param
 
 ## our parameter file:
+rm -rf $BEAGLE_RESULTS/fitting_example/BEAGLE-input-files
 BEAGLE --fit --parameter-file /home/beagle/JPAS_Challenge/fit_photometry_malu_paula.param
